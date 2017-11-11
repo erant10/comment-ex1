280f3eada15f1bf3bd337b2f36a476b6be51b33d
eran <erant10@gmail.com>
2017-11-11 18:33:50 +0200

    Further implementation of the server
    Added a while loop for accepting clients. Implemented delete_file and list_of_files.

--- file_server.c
+++ file_server.c
@@ -1,5 +1,3 @@
-// http://www.binarytides.com/server-client-example-c-sockets-linux/
-
 #include<stdio.h>
 #include<string.h>    //strlen
 #include<sys/socket.h>
@@ -69,65 +67,143 @@ int main(int argc , char *argv[])
 
     //Listen for clients
     listen(socket_desc , 15);
-
-    // maybe need another while loop here
-
     //Accept and incoming connection
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
 
-    //accept connection from an incoming client
-    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-    if (client_sock < 0)
-    {
-        perror("accept failed");
-        return 1;
-    }
-    //Connection established
-    puts("Connection accepted");
+    // keep accepting connections
+    while(1){
 
-    //Receive login info from client
-    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
-    {
-        // parse the client_message according to the protocol
+        //accept connection from an incoming client
+        client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+        if (client_sock < 0) {
+            perror("accept failed");
+            return 1;
+        }
+        //Connection established
+        puts("Connection accepted");
+
+        char *client_login_info;
         char *username, *password;
+        //Keep receiving login info from client until the login is successful
+        while( !successful_login ) {
+            if ((read_size = recv(client_sock, client_login_info, 2000, 0)) > 0) {
+                // parse the client_login_info according to the protocol
+                username = "";
+                password = "";
 
-        // check if the user exists
-
-        // check if password matches
+                // check if the user exists
+                DIR *dir = opendir(username);
+                if (dir) {
+                    // Directory exists - check if password matches
+                    if (password_match) {
+                        successful_login = 1;
+                    }
+                    // closedir when done
+                    closedir(dir);
+                } else if (ENOENT != errno) {
+                    // opendir() failed for some other reason.
+                    perror("opendir failed");
+                    return 1;
+                }
+                if(!successful_login) {
+                    //Send failed login message
+                    char *error_message = "Invalid username or password";
+                    if( send(client_sock , error_message , strlen(error_message) , 0) < 0)
+                    {
+                        puts("Failed sending message");
+                        return 1;
+                    }
+                }
+            }
+            if (read_size == 0) {
+                puts("Client disconnected");
+                fflush(stdout);
+            } else if (read_size < 0) {
+                perror("recv failed");
+            }
+        }
+        // login successful.
+        // count how many files the user has
+        int file_count;
 
         // send welcome message
-
-        // recv command from client
-
-        if (loggedin) {
-            // Implement the actions here
-            if (action == 0) {
-                // list_of_files
-
-            } else if( action == 1 ) {
-                // delete_file
-
-            } else if( action == 2 ) {
-                // add_file
-
-            } else if( action == 3 ) {
-                // get_file
-
-            }
-        } else {
-            // login unsuccessful
-            //Send message back to client
-            write(client_sock , client_message , strlen(client_message));
+        char *welcome_message;
+        sprintf(welcome_message, "Welcome %s, You have %d files.", file_count);;
+        if( send(client_sock , welcome_message , strlen(welcome_message) , 0) < 0) {
+            puts("Failed sending message");
+            return 1;
         }
-    }
+        // start listening for commands
+        char *client_request;
+        while ((read_size = recv(client_sock, client_request, 2000, 0)) > 0) {
+            // read action type from client_request
+            int action;
 
-    if(read_size == 0) {
-        puts("Client disconnected");
-        fflush(stdout);
-    }
-    else if(read_size < 0) {
-        perror("recv failed");
+            switch(action) {
+                case 0 :
+                    // list_of_files
+                    DIR *dir;
+                    struct dirent *ent;
+                    if ((dir = opendir (username)) != NULL) {
+                        char *files_buffer;
+                        char *filename;
+                        // read all the files into a files_buffer
+                        while ((ent = readdir (dir)) != NULL) {
+                            sprintf(filename, "%s\n", ent->d_name);
+                            sprintf(files_buffer + strlen(files_buffer),filename);
+                        }
+                        // finally close dir
+                        closedir (dir);
+
+                        // Send the file list
+                        if( send(client_sock , files_buffer , strlen(files_buffer) , 0) < 0) {
+                            puts("Failed sending message");
+                            return 1;
+                        }
+                    } else {
+                        /* could not open directory */
+                        perror ("");
+                        return EXIT_FAILURE;
+                    }
+                    break;
+                case 1 :
+                    // delete_file
+                    char *filename, *message;
+                    if( access( filename, F_OK ) != -1 ) {
+                        // file exists - remove it
+                        if(remove(filename) == 0) {
+                            message = "File removed.";
+                        } else {
+                            printf("Error: unable to delete the file");
+                        }
+                    } else {
+                        // file doesn't exist
+                        message = "No such file exists!";
+                    }
+                    if( send(client_sock , message , strlen(message) , 0) < 0) {
+                        puts("Failed sending message");
+                        return 1;
+                    }
+                    break;
+                case 2 :
+                    // add_file
+                    break;
+                case 3 :
+                    // get_file
+                    break;
+                default :
+                    // quit
+                    printf("quit");
+            }
+        }
+        if (read_size == 0) {
+            puts("Client disconnected");
+            fflush(stdout);
+        } else if (read_size < 0) {
+            perror("recv failed");
+        }
+
     }
 
     return 0;
